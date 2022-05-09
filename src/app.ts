c7b537e26874a215de254030ecb0586001e1f8cd
DucDung_Tong <56964889+ducdung-tong@users.noreply.github.com>
2022-05-09 09:07:15 +0700

    Fix Cors

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ class App {
     }
     
     private initialiseMiddleware(): void {
-        //this.express.use(cors())
+        this.express.use(cors())
         this.express.use(express.json())
         this.express.use(express.urlencoded({ extended: false }));
     }
