d24bbe030c15f3ad00260721371e14daa0a63a69
DucDung_Tong <56964889+ducdung-tong@users.noreply.github.com>
2022-05-13 14:40:31 +0700

    Modified Post Controller

--- src/resources/controllers/post.controller.ts
+++ src/resources/controllers/post.controller.ts
@@ -1,165 +1,139 @@
-import { Router, Request, Response, NextFunction } from 'express'
-import Controller from '@/utils/interface/controller.interface'
-import HttpException from '@/utils/exceptions/http.exception'
-import validationMiddleware from '@/middleware/validation.midldeware'
-import PostService from '../services/post.service'
+import { Router, Request, Response, NextFunction } from "express"
+import Controller from "@/utils/interface/controller.interface"
+import HttpException from "@/utils/exceptions/http.exception"
+import validationMiddleware from "@/middleware/validation.midldeware"
+import PostService from "../services/post.service"
 
-class PostController implements Controller{
-    public path = "/post"
-    public router = Router()
-    private PostService = new PostService()
+class PostController implements Controller {
+  public path = "/post"
+  public router = Router()
+  private PostService = new PostService()
 
-    constructor(){
-        this.initialiseRoutes()
+  constructor() {
+    this.initialiseRoutes()
+  }
+  private initialiseRoutes() {
+    this.router.post(`${this.path}/upload`, this.create)
+    this.router.get(`${this.path}/get`, this.get)
+    this.router.post(`${this.path}/delete`, this.delete)
+  }
+  private create = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response | void> => {
+    try {
+      const {
+        title,
+        address,
+        ownerId,
+        postTypeId,
+        estateTypeId,
+        forSaleOrRent,
+        status,
+        location,
+        cor,
+        description,
+        images,
+        legalDocuments,
+        publishedDate,
+        expiredDate,
+        price,
+        priceType,
+        area,
+        floorNumber,
+        bathroomNumber,
+        bedroomNumber,
+        direction,
+        furniture,
+        width,
+        depth,
+        roadWidth,
+        facade,
+        belongToProject,
+      } = req.body
+      const token = await this.PostService.create(
+        title,
+        address,
+        ownerId,
+        postTypeId,
+        estateTypeId,
+        forSaleOrRent,
+        status,
+        location,
+        cor,
+        description,
+        images,
+        legalDocuments,
+        publishedDate,
+        expiredDate,
+        expiredDate,
+        expiredDate,
+        price,
+        priceType,
+        area,
+        floorNumber,
+        bathroomNumber,
+        bedroomNumber,
+        direction,
+        furniture,
+        width,
+        depth,
+        roadWidth,
+        facade,
+        belongToProject
+      )
+      res.status(201).json({ token })
+    } catch (error: any) {
+      next(new HttpException(400, error.message))
     }
-    private initialiseRoutes(){
-        this.router.post(
-            `${this.path}/upload`,
-            this.create
+  }
+  private get = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response | void> => {
+    try {
+      let postId = req.query.id?.toString()
+      let status = req.query.s?.toString()
+      let postType = req.query.pt?.toString()
+      let estateType = req.query.et?.toString()
+
+      let data = undefined
+
+      if (status == undefined) status = ""
+      if (postType == undefined) postType = ""
+      if (estateType == undefined) estateType = ""
+
+      if (postId != undefined) {
+        data = await this.PostService.getDetail(postId)
+      } else {
+        data = await this.PostService.getWithParams(
+          status,
+          postType,
+          estateType
         )
-        this.router.get(
-            `${this.path}/get`,
-            this.getDetail
-        )
-        this.router.get(
-            `${this.path}/list-post-by-purpose`,
-            this.getListPostByPurpose
-        )
-        this.router.get(
-            `${this.path}/list-post-by-status`,
-            this.getListPostByStatus
-        )
-        this.router.post(
-            `${this.path}/delete`,
-            this.delete
-        )
-    }
-    private create = async (
-        req: Request,
-        res: Response,
-        next: NextFunction
-    ): Promise<Response | void> => {
-        try {
-            const {
-                title,
-                address,
-                ownerId,
-                postTypeId,
-                estateTypeId,
-                forSaleOrRent,
-                status,
-                location,
-                cor,
-                description,
-                images,
-                legalDocuments,
-                publishedDate,
-                expiredDate,
-                price,
-                priceType,
-                area,
-                floorNumber,
-                bathroomNumber,
-                bedroomNumber,
-                direction,
-                furniture,
-                width,
-                depth,
-                roadWidth,
-                facade,
-                belongToProject
-            } = req.body
-            const token = await this.PostService.create(
-                title,
-                address,
-                ownerId,
-                postTypeId,
-                estateTypeId,
-                forSaleOrRent,
-                status,
-                location,
-                cor,
-                description,
-                images,
-                legalDocuments,
-                publishedDate,
-                expiredDate,
-                expiredDate,
-                expiredDate,
-                price,
-                priceType,
-                area,
-                floorNumber,
-                bathroomNumber,
-                bedroomNumber,
-                direction,
-                furniture,
-                width,
-                depth,
-                roadWidth,
-                facade,
-                belongToProject
-            )
-            res.status(201).json({ token })
-        } catch( error :any ){
-            next(new HttpException(400, error.message))
-        }
-    }
-    private getDetail = async ( 
-        req: Request,
-        res: Response,
-        next: NextFunction
-    ) : Promise<Response | void> => {
-        try {
-            let postId = req.query.id?.toString()
-            const token = await this.PostService.getDetail(postId)
+      }
 
-            res.status(200).json({ token })
-        } catch( error:any ){
-            next(new HttpException(400, error.message))
-        }
+      res.status(200).json({ data })
+    } catch (error: any) {
+      next(new HttpException(400, error.message))
     }
-    private getListPostByPurpose = async (
-        req: Request,
-        res: Response,
-        next: NextFunction
-    ) : Promise<Response | void> => {
-        try {
-            let purpose = req.query.purpose?.toString()
-            const data = await this.PostService.getListPostByPurpose(purpose)
+  }
 
-            res.status(200).json({ data })
-        } catch( error:any ){
-            next(new HttpException(400, error.message))
-        }
-    }
-    private getListPostByStatus = async (
-        req: Request,
-        res: Response,
-        next: NextFunction
-    ) : Promise<Response | void> => {
-        try {
-            let status = req.query.status?.toString()
-            const data = await this.PostService.getListPostByStatus(status)
+  private delete = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response | void> => {
+    try {
+      let postId = req.query.id?.toString()
+      const token = await this.PostService.delete(postId)
 
-            res.status(200).json({ data })
-        } catch( error:any ){
-            next(new HttpException(400, error.message))
-        }
+      res.status(200).json({ token })
+    } catch (error: any) {
+      next(new HttpException(400, error.message))
     }
-    private delete = async (
-        req: Request,
-        res: Response,
-        next: NextFunction
-    ) : Promise<Response | void> => {
-        try {
-            let postId = req.query.id?.toString()
-            const token = await this.PostService.delete(postId)
-
-            res.status(200).json({ token })
-        } catch( error:any ){
-            next(new HttpException(400, error.message))
-        }
-    }
+  }
 }
 export default PostController
--- src/resources/services/post.service.ts
+++ src/resources/services/post.service.ts
@@ -5,6 +5,7 @@ import userModel from "../models/user.model"
 import postTypeModel from "../models/postType.model"
 import estateTypeModel from "../models/estateType.model"
 import priceUnitModel from "../models/priceUnit.model"
+import { string } from "joi"
 
 class PostService {
   private post = PostModel
@@ -117,12 +118,12 @@ class PostService {
       let priceUnit = await this.priceUnit.findById(post?.priceType)
 
       let postDto = parsePostDto(post)
-      postDto.owner.name = owner?.fullname || ''
-      postDto.owner.phone = owner?.phone || ''
-      postDto.postType.name = postType?.name || ''
-      postDto.postType.title_color = postType?.title_color || ''
-      postDto.estateType = estateType?.name || ''
-      postDto.priceType = priceUnit?.label || ''
+      postDto.owner.name = owner?.fullname || ""
+      postDto.owner.phone = owner?.phone || ""
+      postDto.postType.name = postType?.name || ""
+      postDto.postType.title_color = postType?.title_color || ""
+      postDto.estateType = estateType?.name || ""
+      postDto.priceType = priceUnit?.label || ""
 
       if (post) {
         return postDto
@@ -135,9 +136,25 @@ class PostService {
     }
   }
 
-  public async getListPostByPurpose(purpose: any): Promise<any> {
+  public async getWithParams(
+    status: string,
+    postType: string,
+    estateType: string
+  ): Promise<any> {
+
     try {
-      let docs = await this.post.find({ forSaleOrRent: purpose }).exec()
+      let docs = await this.post.find({
+        status: {
+          $regex: new RegExp(status, 'i'),
+        },
+        postTypeId: {
+          $regex: new RegExp(postType, 'i'),
+        },
+        estateTypeId: {
+          $regex: new RegExp(estateType, 'i'),
+        },
+      })
+
       var dataDtos: postDtoInterface[] = []
 
       for (let index = 0; index < docs.length; index++) {
@@ -149,12 +166,12 @@ class PostService {
         let priceUnit = await this.priceUnit.findById(element?.priceType)
 
         let postDto = parsePostDto(element)
-        postDto.owner.name = owner?.fullname || ''
-        postDto.owner.phone = owner?.phone || ''
-        postDto.postType.name = postType?.name || ''
-        postDto.postType.title_color = postType?.title_color || ''
-        postDto.estateType = estateType?.name || ''
-        postDto.priceType = priceUnit?.label || ''
+        postDto.owner.name = owner?.fullname || ""
+        postDto.owner.phone = owner?.phone || ""
+        postDto.postType.name = postType?.name || ""
+        postDto.postType.title_color = postType?.title_color || ""
+        postDto.estateType = estateType?.name || ""
+        postDto.priceType = priceUnit?.label || ""
 
         dataDtos.push(postDto)
       }
@@ -166,40 +183,6 @@ class PostService {
       }
     } catch (error) {
       console.log(error)
-      throw new Error("Unable to get post list")
-    }
-  }
-
-  public async getListPostByStatus(status: any): Promise<any> {
-    try {
-      let docs = await this.post.find({ status: status }).exec()
-      var dataDtos: postDtoInterface[] = []
-
-      for (let index = 0; index < docs.length; index++) {
-        const element = docs[index]
-
-        let owner = await this.user.findById(element?.ownerId)
-        let postType = await this.postType.findById(element?.postTypeId)
-        let estateType = await this.estateType.findById(element?.estateTypeId)
-        let priceUnit = await this.priceUnit.findById(element?.priceType)
-
-        let postDto = parsePostDto(element)
-        postDto.owner.name = owner?.fullname || ''
-        postDto.owner.phone = owner?.phone || ''
-        postDto.postType.name = postType?.name || ''
-        postDto.postType.title_color = postType?.title_color || ''
-        postDto.estateType = estateType?.name || ''
-        postDto.priceType = priceUnit?.label || ''
-
-        dataDtos.push(postDto)
-      }
-
-      if (docs) {
-        return dataDtos
-      } else {
-        throw new Error("Cannot get post list")
-      }
-    } catch (error) {
       throw new Error("Unable to get post list")
     }
   }
