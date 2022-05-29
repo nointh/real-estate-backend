e96bb2e6a7583c108e38e1fcc92c7ae61907fdee
DucDung_Tong <tngcdng@gmail.com>
2022-05-29 10:28:15 +0700

    Modify Trans API

--- src/resources/services/transaction.service.ts
+++ src/resources/services/transaction.service.ts
@@ -1,8 +1,10 @@
 import transactionModel from "../models/transaction.model"
 import ITransaction from "../interfaces/transaction.interface"
+import userModel from "../models/user.model"
 
 class TransactionService {
   private transaction = transactionModel
+  private user = userModel
 
   public async get(id: string): Promise<any> {
     try {
@@ -16,7 +18,7 @@ class TransactionService {
 
   public async getWithParams(id: string, type: string): Promise<any> {
     try {
-      const trans = await this.transaction
+      let trans = await this.transaction
         .find({
           user: {
             $regex: new RegExp(id, "i"),
@@ -25,15 +27,28 @@ class TransactionService {
             $regex: new RegExp(type, "i"),
           },
         })
-        .sort({ dateProceed: 1 })
+        .sort({ dateProceed: -1 })
 
-      return trans
+      var newTrans: ITransaction[] = []
+
+      for (let index = 0; index < trans.length; index++) {
+        let element = trans[index]
+
+        let _user = await this.user.findById(element?.user)
+        if (_user) {
+          let username = _user.username
+          element.user = username
+          newTrans.push(element)
+        }
+      }
+
+      return newTrans
     } catch (error) {
       throw new Error("Unable to get transaction")
     }
   }
 
-  public async finish(id: string, status: string): Promise<any> {
+  public async finish(id: string, status: string, balance?: number): Promise<any> {
     try {
       let result = await this.transaction.updateOne(
         { id: id },
@@ -41,13 +56,14 @@ class TransactionService {
           $set: {
             status: status,
             dateFinish: new Date(),
+            balance: balance
           },
         }
       )
 
       return result
     } catch (error) {
-      throw new Error("Unable to get transaction")
+      throw new Error("Unable to finish transaction")
     }
   }
 
